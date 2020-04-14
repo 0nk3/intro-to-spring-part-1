42a67e9cf49101824bbb08558ee6cc96153c771f
Ndumiso Onke Fanti <0nk3.0nk3@gmail.com>
2020-04-14 21:38:41 +0200

    removal of my demo file

--- src/main/java/com/onke/spring/springboot/UserService.java
+++ src/main/java/com/onke/spring/springboot/UserService.java
@@ -3,5 +3,5 @@ package com.onke.spring.springboot;
 public interface UserService {
     String addUser(long id, String name, String surname);
     void remove(long id);
-    void getUser(long id);
+    long getUser(long id);
 }
--- src/main/java/com/onke/spring/springboot/UserServiceImpl.java
+++ src/main/java/com/onke/spring/springboot/UserServiceImpl.java
@@ -2,9 +2,25 @@ package com.onke.spring.springboot;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
-public class UserServiceImpl {
+public class UserServiceImpl implements UserService {
 
     @Autowired
     FakeRepo fakeRepo;
 
+    @Override
+    public String addUser(long id, String name, String surname) {
+        fakeRepo.insertUser(1, "Linus","Torvalds");
+        return name;
+    }
+
+    @Override
+    public void remove(long id) {
+        fakeRepo.deleteUser(1);  // removes Linus
+    }
+
+    @Override
+    public long getUser(long id) {
+        fakeRepo.findById(1);
+        return id;
+    }
 }
