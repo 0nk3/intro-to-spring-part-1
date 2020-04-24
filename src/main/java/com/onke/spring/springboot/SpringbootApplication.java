ae3d4ea407f257d0c74dcc1754dc37b6712fa1ec
0nk3 <53526231+0nk3@users.noreply.github.com>
2020-04-24 22:39:02 +0200

    bug fix

--- src/main/java/com/onke/spring/springboot/SpringbootApplication.java
+++ src/main/java/com/onke/spring/springboot/SpringbootApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan
+@SpringBootApplication
 public class SpringbootApplication {
 
     public static void main(String[] args) {
